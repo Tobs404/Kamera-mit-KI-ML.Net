5b879bc59993926ed2be137fdcea5c4a1b3195f0
lbsaccount <terrariaxbox166@gmail.com>
2024-03-29 09:46:08 +0100

    Add project files.

---
+++ WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿
+namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pictureBox1 = new System.Windows.Forms.PictureBox();
+            pictureBox2 = new System.Windows.Forms.PictureBox();
+            button1 = new System.Windows.Forms.Button();
+            button2 = new System.Windows.Forms.Button();
+            button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new System.Drawing.Point(13, 68);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new System.Drawing.Size(438, 381);
+            pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Location = new System.Drawing.Point(510, 68);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new System.Drawing.Size(438, 381);
+            pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            pictureBox2.TabIndex = 1;
+            pictureBox2.TabStop = false;
+            // 
+            // button1
+            // 
+            button1.Location = new System.Drawing.Point(221, 26);
+            button1.Name = "button1";
+            button1.Size = new System.Drawing.Size(134, 36);
+            button1.TabIndex = 2;
+            button1.Text = "Start Camera";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click_1;
+            // 
+            // button2
+            // 
+            button2.Location = new System.Drawing.Point(420, 26);
+            button2.Name = "button2";
+            button2.Size = new System.Drawing.Size(134, 36);
+            button2.TabIndex = 3;
+            button2.Text = "Capture";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click_1;
+            // 
+            // button4
+            // 
+            button4.Location = new System.Drawing.Point(600, 26);
+            button4.Name = "button4";
+            button4.Size = new System.Drawing.Size(134, 36);
+            button4.TabIndex = 5;
+            button4.Text = "Prediction";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(969, 466);
+            Controls.Add(button4);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(pictureBox2);
+            Controls.Add(pictureBox1);
+            Name = "Form1";
+            Text = "ML Learning";
+            //Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ WinFormsApp1/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.IO;
+using System.Drawing;
+using System.Windows.Forms;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using WinFormsApp1ML.Model;
+
+namespace WinFormsApp1
+{   /*
+    Kurzes Vorab das hier ist 2019 ML.Net Version
+    Das ist Version 1/2 mit Manuelem Caputre andere Version hat einen einfachen Timer und macht alle 3 sek ein Fotot und Predicted
+    Dieses KI ist die auf dem neuesten Stand
+    Andere Version wird nicht mitkomentiert da sehr ähnlich bis auf Timer
+    Scource Code für beide auf Git Link dazu im offiziellen Doc.
+    */
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent(); // initialisiert componenten wie Butoon etc.
+        }
+
+        VideoCaptureDevice CAMERA;
+        string saveDirectory = @"C:\Users\z004tyry\Downloads"; //wo Daten gespeichert werden sollen
+        string fileName = "TmpPic.jpg"; //wie die Temp File gespeichert wird
+
+
+        private void FmWebcamera_Load(object sender, EventArgs e)
+        {
+            // Vorbereiten der Kamera
+        }
+
+        private void Captured(object sender, NewFrameEventArgs eventArgs) // Umwandlung die Bitmap und weitergabe
+        {
+            Bitmap bmp;
+            bmp = (Bitmap)eventArgs.Frame.Clone();
+            pictureBox1.Image = (eventArgs.Frame.Clone() as Bitmap);
+        }
+
+        private void FmWebcamera_FormClosing(object sender, FormClosingEventArgs e) // Stoppen der Kamera damit man rescourcen sparren kann
+        {
+            if (CAMERA != null && CAMERA.IsRunning)
+            {
+                CAMERA.Stop();
+            }
+
+            string filePath = Path.Combine(saveDirectory, fileName);
+
+            if (File.Exists(filePath)) // Falls die File vorhanden ist wird sie hier gelöscht
+            {
+                try
+                {
+                    File.Delete(filePath);
+                    MessageBox.Show("Temporary file deleted.");
+                }
+                catch (IOException ioExp)
+                {
+                    MessageBox.Show($"Error deleting temporary file: {ioExp.Message}");
+                }
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e) // eventhandler für button mit Namen Start der die Kamera startetd und das select Menu öffnet
+        {
+            VideoCaptureDeviceForm camera = new VideoCaptureDeviceForm();
+            if (camera.ShowDialog() == DialogResult.OK)
+            {
+                CAMERA = camera.VideoDevice;
+                CAMERA.NewFrame += new NewFrameEventHandler(Captured);
+                CAMERA.Start();
+            }
+        }
+
+        private void button2_Click_1(object sender, EventArgs e) // Caputre Button der die das Gewünschte Bild Cloned
+        {
+            pictureBox2.Image = pictureBox1.Image;
+        }
+
+
+        private void button4_Click(object sender, EventArgs e) // Eventhandler für Button Predict hier wird der KI bescheid gegeben das sie erraten soll was im Bild ist
+        {
+
+            if (!Directory.Exists(saveDirectory))
+            {
+                Directory.CreateDirectory(saveDirectory);
+            }
+
+            string filePath = Path.Combine(saveDirectory, fileName);
+
+            pictureBox2.Image.Save(filePath, System.Drawing.Imaging.ImageFormat.Jpeg);
+
+            if (File.Exists(filePath))
+            {
+                var input = new ModelInput { ImageSource = filePath }; // hier started die KI
+                var result = ConsumeModel.Predict(input);               //output
+
+                MessageBox.Show($"Prediction: {result.Prediction}");
+            }
+            else
+            {
+                MessageBox.Show("Image not found. Please capture and save an image first."); // error Handling
+                
+            }
+        }
+    }
+}
